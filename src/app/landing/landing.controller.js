81ce445b3d0d6329eeb4724e83c277010c5c5182
wesley <wesley.wang@sk8.asia>
2017-06-02 13:18:14 +1000

    update landing

--- src/app/landing/landing.controller.js
+++ src/app/landing/landing.controller.js
@@ -1,28 +1,31 @@
 export class LandingController {
-  constructor ($scope, webDevTec, toastr) {
+  constructor($scope, webDevTec, toastr, $state) {
     'ngInject';
+    this.onDraggableEvent = this.onDraggableEvent.bind(this);
     $scope.$on('draggable:start', this.onDraggableEvent);
     $scope.$on('draggable:end', this.onDraggableEvent);
     this.droppedObjects1 = [{name: 'aaA'}];
-
-
+    this.state = $state;
+    this.onDrag = false;
 
   }
 
   onDraggableEvent(evt, data) {
+    this.onDrag = true;
     console.log("128", "onDraggableEvent", evt, data);
   }
 
-  onDragComplete(data,evt){
+  onDragComplete(data, evt) {
     var index = this.droppedObjects1.indexOf(data);
     if (index == -1)
       this.droppedObjects1.push(data);
-}
-  onDropComplete(data,evt){
+  }
 
+  onDropComplete(data, evt) {
     var index = this.droppedObjects1.indexOf(data);
     if (index > -1) {
       this.droppedObjects1.splice(index, 1);
+      this.state.go('home');
     }
 
   }
