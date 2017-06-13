7b29da4ae9af46d3c11332326310ae0ca042efd3
wesley <wesley.wang@sk8.asia>
2017-06-13 17:49:34 +1000

    minor change

--- src/app/index.service.js
+++ src/app/index.service.js
@@ -54,6 +54,9 @@ export class MainService {
       }
 
     };
+
+
+
   }
 
 }
--- src/app/landing/landing.controller.js
+++ src/app/landing/landing.controller.js
@@ -1,5 +1,5 @@
 export class LandingController {
-  constructor($scope, webDevTec, toastr, $state, $window, $document) {
+  constructor($scope, webDevTec, toastr, $state, $window) {
     'ngInject';
     var myWindow = angular.element($window);
     this.onDraggableEvent = this.onDraggableEvent.bind(this);
@@ -11,29 +11,29 @@ export class LandingController {
     // $window.keyup(function(e) { d[e.which] = false; });
     this.onDrag = false;
 
-    var pane = $document.find('#pane'),
-      box = $document.find('#box'),
-      d = {},
+    var pane = document.getElementById("pane"),
+      box = document.getElementById("box"),
+
       x = 3;
     this.box = box;
-    this.w = pane.width() - box.width(),
-    console.log(w)
+    this.d = {};
+    this.w = pane.style.width - box.style.width;
 
+    var that = this;
     myWindow.on("keydown", function(evt) {
-      d[evt.which] = true;
-
-      console.log($document.find('#box').width())
+      console.log(1, evt.which)
+      that.d[evt.which] = true;
     });
 
     myWindow.on("keyup", function(evt) {
-      d[evt.which] = false;
+      that.d[evt.which] = false;
     });
 
-    setInterval(function() {
-      this.box.css({
-        left: function(i,v) { return newv(v, 37, 39); },
-        top: function(i,v) { return newv(v, 38, 40); }
-      });
+    setInterval(function(){
+      document.getElementById("box").style.left = function(i,v) { return this.newv(v, 37, 39); };
+      console.log(document.getElementById("box").style.left)
+      window.document.getElementById("box").style.top = function(i,v) { return this.newv(v, 38, 40); };
+
     }, 20)
 
   }
@@ -41,7 +41,8 @@ export class LandingController {
 
 
   newv(v,a,b) {
-    var n = parseInt(v, 10) - (d[a] ? x : 0) + (d[b] ? x : 0);
+    console.log(2);
+    var n = parseInt(v, 10) - (this.d[a] ? 3 : 0) + (this.d[b] ? 3 : 0);
     return n < 0 ? 0 : n > this.w ? this.w : n;
   }
 
